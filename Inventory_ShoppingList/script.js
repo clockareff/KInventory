2155611e8eba75aa6aa46eb09c8cce6a26f00af3
Bzimm01 <bzimm@umich.edu>
2016-12-15 21:00:38 -0500

    added 'add item' feature, and 'edit item' feature

--- Inventory_ShoppingList/script.js
+++ Inventory_ShoppingList/script.js
@@ -6,13 +6,31 @@ app.controller('inventory', ['$scope', '$http', function($scope, $http){
 		expDate: '12/20/16',
 		foodGroup: 'Dairy',
 		location: 'Refridgerator',
+		id: '0'
 	}, {
 		name: 'Spam',
 		expDate: 'N/A',
 		foodGroup: 'Canned Meat',
 		location: 'Pantry',
+		id: '1'
 	}];
+	$scope.idCount = $scope.inventoryArray.length;
+	$scope.newItem = {};
+	$scope.updateItem = function(item){
+		for(var i = 0; i < $scope.inventoryArray.length; i++){
+			if (item.id == $scope.inventoryArray[i].id){
+				$scope.inventoryArray[i] = item;
+				break;
+			}
+		}
+		//Write to JSON -- TODO --
+	};
 	$scope.addItem = function(item){
+		item.id = $scope.idCount;
+		$scope.idCount++;
 		$scope.inventoryArray.push(item);
-	}
+		//SORT -- TODO --
+		//Write to JSON -- TODO --
+		$scope.newItem = {};
+	};
 }]);
