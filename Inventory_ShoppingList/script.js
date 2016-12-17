2c8da25e52c7e13957cdbbd01675e66dbee1449f
bboudrea <bboudrea@umich.edu>
2016-12-17 15:57:54 -0500

    fixed SL page, added SL controller

--- Inventory_ShoppingList/script.js
+++ Inventory_ShoppingList/script.js
@@ -6,14 +6,12 @@ app.controller('inventory', ['$scope', '$http', function($scope, $http){
 		expDate: '12/20/16',
 		foodGroup: 'Dairy',
 		location: 'Refridgerator',
-		storeLocation: 'Dairy Aisle',
 		id: '0'
 	}, {
 		name: 'Spam',
 		expDate: 'N/A',
 		foodGroup: 'Canned Meat',
 		location: 'Pantry',
-		storeLocation: 'Canned Food',
 		id: '1'
 	}];
 	$scope.idCount = $scope.inventoryArray.length;
@@ -31,6 +29,37 @@ app.controller('inventory', ['$scope', '$http', function($scope, $http){
 		item.id = $scope.idCount;
 		$scope.idCount++;
 		$scope.inventoryArray.push(item);
+		//SORT -- TODO --
+		//Write to JSON -- TODO --
+		$scope.newItem = {};
+	};
+}]);
+
+app.controller('shoppingList', ['$scope', '$http', function($scope, $http){
+	$scope.sLArray = [{
+		name: 'Cheese',
+		storeLocation: 'Dairy Aisle',
+		id: '0'
+	}, {
+		name: 'Oatmeal',
+		storeLocation: 'Breakfast Foods',
+		id: '1'
+	}];
+	$scope.idCount = $scope.sLArray.length;
+	$scope.newItem = {};
+	$scope.updateItem = function(item){
+		for(var i = 0; i < $scope.sLArray.length; i++){
+			if (item.id == $scope.sLArray[i].id){
+				$scope.sLArray[i] = item;
+				break;
+			}
+		}
+		//Write to JSON -- TODO --
+	};
+	$scope.addItem = function(item){
+		item.id = $scope.idCount;
+		$scope.idCount++;
+		$scope.sLArray.push(item);
 		//SORT -- TODO --
 		//Write to JSON -- TODO --
 		$scope.newItem = {};
